b730c3bb70ddf034bd076419095de1d73daae275
soyeon <uyyu1734@gmail.com>
2024-07-19 08:27:00 +0900

    chore: gitignore

---
+++ src/main/java/com/example/summer_comming/trip/Accomodation.java
@@ -0,0 +1,10 @@
+package com.example.summer_comming.trip;
+
+public class Accomodation {
+    String name;
+    String location;
+    String phoneNumber;
+    int minimumPrice;
+
+    public Accomodation() {}
+}
---
+++ src/main/java/com/example/summer_comming/trip/Room.java
@@ -0,0 +1,7 @@
+package com.example.summer_comming.trip;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Room {
+}
