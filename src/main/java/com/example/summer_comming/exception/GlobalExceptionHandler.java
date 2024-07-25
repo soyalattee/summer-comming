274e43fbc3a697464678f86f084036366f6893ca
soyeon <uyyu1734@gmail.com>
2024-07-25 09:57:47 +0900

    Feat: 예외가 전역에서 처리되도록 수정
    RestControllerAdvice를 생성하여 발생한 에러를 전역에서 처리 (공통 에러로직들을 해당 핸들러에서 처리할 수있음)

--- src/main/java/com/example/summer_comming/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/summer_comming/exception/GlobalExceptionHandler.java
@@ -1,8 +1,15 @@
 package com.example.summer_comming.exception;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Controller //모든 컨트롤러에 예외가 발생하면 낚아챌게~
+@RestControllerAdvice //모든 컨트롤러에 예외가 발생하면 낚아챌게~
 public class GlobalExceptionHandler {
-
+    //    해당 예외가 발생하면 여기서 처리할게!
+    @ExceptionHandler(value = RoomNotFoundException.class) //value를 안넣어줘도 스프링이 파라미터를 보고 예측해서 처리해줄 수 있음.
+    public String catchRoomNotFoundException(RoomNotFoundException e) {
+        System.out.println("에러 커밍"+e.getClass());
+        return "No Room Found";
+    }
 }
--- src/main/java/com/example/summer_comming/room/RoomController.java
+++ src/main/java/com/example/summer_comming/room/RoomController.java
@@ -28,10 +28,4 @@ public class RoomController {
         return roomRes;
 //        return new ResponseEntity<>(roomRes, HttpStatus.CREATED);
     }
-    //    해당 예외가 발생하면 여기서 처리할게!
-    @ExceptionHandler(value = RoomNotFoundException.class) //value를 안넣어줘도 스프링이 파라미터를 보고 예측해서 처리해줄 수 있음.
-    public String catchRoomNotFoundException(RoomNotFoundException e) {
-        System.out.println("에러 커밍"+e.getClass());
-        return "No Room Found";
-    }
 }
